6653d7f5913a30b24ee339d66d9bf8bd79ac8c52
Federico <codepalace@fastmail.se>
2022-03-30 14:16:06 +0200

    Initial Commit

---
+++ MVVMPokedex/MVVMPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMPokedexApp.swift
+//  MVVMPokedex
+//
+//  Created by Federico on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
